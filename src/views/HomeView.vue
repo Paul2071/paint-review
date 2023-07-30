9cdd793d9a847d62cfd18482ec8eb73d4873eaa7
Paul2071 <101050756+Paul2071@users.noreply.github.com>
2023-07-30 22:05:07 +0100

    added a setup function which fires before other life cycle hooks - in it you can declare variables, have functions etc

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="home">
-    
+    <h1>homepage</h1>
+
+    <p> my name is {{ name }} and i am {{ age }}</p>
   </div>
 </template>
 
@@ -8,6 +10,12 @@
 
 export default {
   name: 'HomeView',
+  setup() {
+    let name = "mario"
+    let age = 30
+
+    return {  name, age}
+  }
   
 }
 </script>
