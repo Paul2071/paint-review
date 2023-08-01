cd2c43b26fe6f7a9ec3f91eb31f32f24efc3183f
Paul2071 <101050756+Paul2071@users.noreply.github.com>
2023-08-01 13:46:52 +0100

    playing around with refs - button to log p. p is returned at setup as a ref hook. p tag is given ref attribute of p. console log p. It works!

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,19 +2,31 @@
   <div class="home">
     <h1>homepage</h1>
 
-    <p> my name is {{ name }} and i am {{ age }}</p>
+    <p ref="p"> my name is {{ name }} and i am {{ age }}</p>
+    <button @click="handleClick">click me</button>
   </div>
 </template>
 
 <script>
 
+import {ref} from "vue"
+
 export default {
   name: 'HomeView',
   setup() {
+
+    const p = ref(null)
+
+    
     let name = "mario"
     let age = 30
+    
+    const handleClick =  () => {
+      console.log(p)
+    }
 
-    return {  name, age}
+
+    return {  name, age, handleClick, p }
   }
   
 }
